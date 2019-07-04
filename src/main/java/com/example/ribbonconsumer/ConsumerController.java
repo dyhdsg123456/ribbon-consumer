cf4243fb73624608df2c66219fd43dc43d6b6c6d
duyuanhao <13775516233@163.com>
2019-07-04 17:45:39 +0800

    1

--- src/main/java/com/example/ribbonconsumer/ConsumerController.java
+++ src/main/java/com/example/ribbonconsumer/ConsumerController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+
 
 @RestController
 public class ConsumerController {
@@ -15,5 +17,12 @@ public class ConsumerController {
     @RequestMapping(value = "/ribbon-consumer",method = RequestMethod.GET)
     public String helloConsumer(){
         return restTemplate.getForEntity("http://HELLO-SERVICE/hello3",String.class).getBody();
+    }
+
+    @RequestMapping(value = "/getuser",method = RequestMethod.GET)
+    public User getuser(){
+        HashMap<String, String> param = new HashMap<>();
+        param.put("name","dyh4");
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello4?name={name}",User.class,param).getBody();
     }
 }
---
+++ src/main/java/com/example/ribbonconsumer/User.java
@@ -0,0 +1,18 @@
+package com.example.ribbonconsumer;
+
+/**
+ * Author: dyh
+ * Date:   2019/7/4
+ * Description:
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
