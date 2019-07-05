39e737e87015443fd0f380f8a637827a6d9fa3db
duyuanhao <13775516233@163.com>
2019-07-05 09:44:42 +0800

    1

--- src/main/java/com/example/ribbonconsumer/ConsumerController.java
+++ src/main/java/com/example/ribbonconsumer/ConsumerController.java
@@ -3,6 +3,7 @@ package com.example.ribbonconsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,9 +21,12 @@ public class ConsumerController {
     }
 
     @RequestMapping(value = "/getuser",method = RequestMethod.GET)
+    @ResponseBody
     public User getuser(){
         HashMap<String, String> param = new HashMap<>();
         param.put("name","dyh4");
-        return restTemplate.getForEntity("http://HELLO-SERVICE/hello4?name={name}",User.class,param).getBody();
+//        return restTemplate.getForEntity("http://HELLO-SERVICE/hello4?name={name}",User.class,param).getBody();
+        User user = restTemplate.getForObject("http://HELLO-SERVICE/hello4?name={name}", User.class, param);
+        return user;
     }
 }
